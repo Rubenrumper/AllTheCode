59b1742d5f64e029f83c024a46f9a7094fc19063
Alexander Antonsen <alex754f@mail.hansenberg.dk>
2024-03-19 08:47:34 +0100

    we

--- 01_Op/obj/Debug/net8.0/AllMyCode.AssemblyInfo.cs
+++ 01_Op/obj/Debug/net8.0/AllMyCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllMyCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572ef9adbd0e0ff049271ebce8a3879109ef0860")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1f4ccb9948c173b257dc0bc6b1803d23f5d27e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllMyCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllMyCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
